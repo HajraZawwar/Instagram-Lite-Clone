587dc9bc5ecef4d95fc4a842d76c75c9bce6d7ab
HajraZawwar <130221854+HajraZawwar@users.noreply.github.com>
2025-05-06 13:08:46 +0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,13 +1,10 @@
-import pytest
-from app import app as flask_app
+from config import Config
 
-@pytest.fixture
-def app():
-    flask_app.config.update({
-        "TESTING": True,
-    })
-    yield flask_app
-
-@pytest.fixture
-def client(app):
-    return app.test_client()
+def test_config():
+    assert Config.MONGO_URI is not None, "MONGO_URI is not set"
+    assert Config.AWS_ACCESS_KEY_ID is not None, "AWS_ACCESS_KEY_ID is not set"
+    assert Config.AWS_SECRET_ACCESS_KEY is not None, "AWS_SECRET_ACCESS_KEY is not set"
+    assert Config.AWS_REGION is not None, "AWS_REGION is not set"
+    assert Config.S3_BUCKET_NAME is not None, "S3_BUCKET_NAME is not set"
+    assert Config.SECRET_KEY is not None, "SECRET_KEY is not set"
+    print("All configuration variables are set.")
