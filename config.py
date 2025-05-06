9f2df009a3f0f5c688f6ac6b4a227c3820511078
HajraZawwar <130221854+HajraZawwar@users.noreply.github.com>
2025-05-06 13:08:11 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,28 +1,18 @@
 import os
 from dotenv import load_dotenv
-import boto3
-from pymongo import MongoClient
 
 load_dotenv()
 
-# MongoDB
-mongo_uri = os.getenv('MONGO_URI')
-mongo_client = MongoClient(mongo_uri)
-db = mongo_client['insta_db']  # Or your actual db name
+class Config:
+    # MongoDB
+    MONGO_URI = os.getenv("MONGO_URI")
 
-# AWS S3
-s3 = boto3.client(
-    's3',
-    aws_access_key_id=os.getenv('AKIAZURXUCXMN2EDQ55Q'),
-    aws_secret_access_key=os.getenv('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'),
-    region_name=os.getenv('Europe (Stockholm) eu-north-1')
-)
-bucket_name = os.getenv('myawsbucket8777')
+    # AWS
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    AWS_REGION = os.getenv("AWS_REGION")
+    S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 
-MONGO_URI = os.getenv("mongodb+srv://hajrazawwarmalik:<db_password>@instagramlitecluster.tlxvfb7.mongodb.net/?retryWrites=true&w=majority&appName=InstagramLiteCluster")
-S3_BUCKET_NAME = os.getenv("myawsbucket8777")
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_REGION = os.getenv('AWS_REGION')
-SECRET_KEY = os.getenv('SECRET_KEY')
-USE_DYNAMODB = os.getenv("USE_DYNAMODB", "False") == "True"
+    # Flask
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    USE_DYNAMODB = os.getenv("USE_DYNAMODB", "False") == "True"
